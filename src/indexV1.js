56d87e80f6c8de35b855d0c9e90624f456b96465
Andrew Stanton <up683693@myport.ac.uk>
2020-02-13 08:46:24 +0000

    Revert "Removed old file"
    This reverts commit 372fbed3de08a108c908efaea18e5e0fb68c023f.

---
+++ src/indexV1.js
@@ -0,0 +1,33 @@
+import { Fef } from './fef/fef';
+
+const processor = new Fef('../../data/in/csvEmail.csv', 'csv');
+
+processor.setInputFilter(
+	(academic) =>
+		academic['Name variant > Known as name-0'] &&
+		academic['UUID-1'] &&
+		academic['Organisations > Email addresses > Email-2']
+);
+
+processor.setItemTransformation((academic) => {
+	const baseURL = 'https://researchportal.port.ac.uk/portal/en/persons/';
+
+	/** Format the name for the URL
+	 *
+	 * @param {string} name The original name
+	 * @returns {string} URL compatible name
+	 */
+	const normaliseAcademicName = (name) =>
+		name.replace(/\s/g, '-').toLowerCase();
+
+	const name = academic['Name variant > Known as name-0'];
+	const uuid = academic['UUID-1'];
+	const email = academic['Organisations > Email addresses > Email-2'];
+
+	const url = `${baseURL}${normaliseAcademicName(
+		name
+	)}(${uuid})/publications.html`;
+	return { url, name, uuid, email };
+});
+
+processor.run();
