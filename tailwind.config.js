ad60e56d371fdb042abcca844b3cec0b16f03e21
Andrew Stanton <up683693@myport.ac.uk>
2020-03-11 16:45:16 +0000

    Saving progress end of day
    Starting to add Fef

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	theme: {
+		fontFamily: {
+			header: '"Encode Sans Expanded", sans-serif',
+			sans: '"Open Sans", sans-serif',
+		},
+		extend: {
+			colors: {
+				'purple-dark': '#3C023C',
+				'purple-light': '#621360',
+				'grey-dark': '#505457',
+				'grey-mid': '#abaaaa',
+				'background-panel': '#ededed',
+				'blue-dark': '#0078b4',
+				'blue-light': '#00a0ff',
+			},
+		},
+	},
+};
