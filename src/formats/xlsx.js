ee5770ff0531917b6a29187cb1699f3793295abe
Andrew Stanton <up683693@myport.ac.uk>
2020-03-16 11:08:41 +0000

    Using XLSX to export a spreadsheet

--- src/formats/xlsx.js
+++ src/formats/xlsx.js
@@ -1,7 +1,7 @@
 import XLSX from 'xlsx';
 
 export const xlsxToJSON = (data) => {
-	const {Sheets, SheetNames} = XLSX.read(data, { type: 'binary'});
+	const { Sheets, SheetNames } = XLSX.read(data, { type: 'binary' });
 	const sheet = Sheets[SheetNames];
 
 	const jsonSheet = XLSX.utils.sheet_to_json(sheet, {
@@ -10,4 +10,16 @@ export const xlsxToJSON = (data) => {
 	console.log(jsonSheet);
 	return jsonSheet;
 };
-export const jsonToXLSX = () => {};
+export const jsonToXLSX = (data) => {
+	/* original data */
+	const filename = 'write.xlsx';
+	const ws_name = 'SheetJS';
+
+	const wb = XLSX.utils.book_new();
+	const ws = XLSX.utils.json_to_sheet(data);
+
+	XLSX.utils.book_append_sheet(wb, ws, ws_name);
+
+	XLSX.writeFile(wb, filename);
+	return null;
+};
